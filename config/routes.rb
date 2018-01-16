a4ae28c1654c5154fd7d6f1c77762fae308198f1
Alessandro <argferreira1@gmail.com>
2018-01-16 20:42:50 -0200

    User model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
   root 'static_pages#index'
 
   get '/help',   to: 'static_pages#help'
+
+  resources :users
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
