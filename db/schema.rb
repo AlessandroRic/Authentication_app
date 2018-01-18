d4212df9eafd94028b1472131b239a426684c5cd
Alessandro <argferreira1@gmail.com>
2018-01-18 14:26:48 -0200

    attributes validation

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20180117003928) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: 'index_users_on_email', unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
---
+++ spec/factories/user.rb
@@ -0,0 +1,17 @@
+FactoryGirl.define do
+  factory :user do
+    first_name 'John'
+    last_name 'Carter'
+    email 'john@example.com'
+    password 'foobar'
+    password_confirmation 'foobar'
+
+  factory :invalid_user do
+    first_name 'J'
+    last_name 'Carter'
+    email 'john_at_example'
+    password 'foo'
+    password_confirmation 'bar'
+    end
+  end
+end
