cb225bb17f7f0700940934f67fca235d4b19aad3
Alessandro <argferreira1@gmail.com>
2018-01-19 09:49:29 -0200

    login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root 'static_pages#index'
 
-  get '/help',   to: 'static_pages#help'
+  get '/help',     to: 'static_pages#help'
 
-  get 'signup',  to: 'users#new'
+  get 'signup',   to: 'users#new'
+
+  get '/login',   to: 'sessions#new'
+
+  post '/login',   to: 'sessions#create'
+
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
 
