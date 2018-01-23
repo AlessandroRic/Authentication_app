1156991746717be8be806f53ecc7edbd3cc217ea
Alessandro <argferreira1@gmail.com>
2018-01-23 15:52:47 -0200

    Authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def index
     
   end
@@ -39,6 +43,18 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:alert] = 'Pleasse log in'
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url unless @user == current_user
   end
 
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -6,12 +6,20 @@ FactoryGirl.define do
     password 'foobar'
     password_confirmation 'foobar'
 
-  factory :invalid_user do
+    factory :other_user do
+      first_name 'Flash'
+      last_name 'Gordon'
+      email 'flash@example.com'
+      password 'foobar'
+      password_confirmation 'foobar'
+
+      factory :invalid_user do
     first_name 'J'
     last_name 'Carter'
     email 'john_at_example'
     password 'foo'
     password_confirmation 'bar'
+    end
     end
   end
 end
